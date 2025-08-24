1ec3d34925e1863769324e0694610a64b9e94dc2
Nolwazi Ngcobo <nollyziengcobo@gmail.com>
2025-08-24 21:04:46 +0200

    Update coke.py

--- coke.py
+++ coke.py
@@ -1,14 +1,17 @@
+#price of the coke bottle and your starting total
 price = 50
 total = 0
 
+#loop while the condition is true
 while total < price:
     print(f'Amount Due: {price - total}')
     coin = int(input('insert coin: '))
 
-
+    #accepted coins
     if coin in [25, 10, 5]:
         total += coin
 
+    #ignore other coins
     else:
         continue
 print(f'change Owed: {total - price}')
